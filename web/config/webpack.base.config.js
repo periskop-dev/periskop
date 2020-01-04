423c023019e7796b77ce20496000d8114844236d
jcreixell <jcreixell@gmail.com>
2020-01-04 21:12:22 +0100

    Upgrade Webpack and related dependencies
      - Upgrades webpack to 4.x
      - This is required for #38
      - Fixes an issue with webpack's dev server that prevented it from
      running.

--- web/config/webpack.base.config.js
+++ web/config/webpack.base.config.js
@@ -63,7 +63,7 @@ module.exports = function (env) {
                     test: /\.less$/,
                     use: [
                       {
-                        loader: "style-loader",
+                        loader: 'style-loader', // creates style nodes from JS strings
                       },
                       {
                         loader: 'css-loader', // translates CSS into CommonJS
