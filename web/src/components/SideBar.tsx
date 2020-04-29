5c22ac91cb4cb169e0bcfa72251debc87f20f586
Tiago Ferreira <sikozonbatata@gmail.com>
2020-04-29 18:38:29 +0100

    implements errors sorting

--- web/src/components/SideBar.tsx
+++ web/src/components/SideBar.tsx
@@ -1,15 +1,16 @@
 import "SideBar.scss"
 import * as React from "react";
-import { ListGroup, Badge } from "react-bootstrap";
+import { ListGroup, Badge, DropdownButton, ButtonGroup, Dropdown } from "react-bootstrap";
 
 import { bindActionCreators, Dispatch, AnyAction } from "redux"
 import { connect } from "react-redux"
-import { StoreState, AggregatedError } from "data/types"
-import { setActiveError } from "data/errors"
+import { StoreState, AggregatedError, SortFilters } from "data/types"
+import { setActiveError, setActiveErrorSortFilter } from "data/errors"
 import { RouteComponentProps, withRouter } from "react-router";
 
 interface DispatchProps {
   setActiveError: (notifcation: string) => void
+  setActiveErrorSortFilter: (filter: SortFilters) => void,
 }
 
 interface ConnectedProps {
@@ -18,10 +19,17 @@ interface ConnectedProps {
 
 interface DefaultsProps {
   errors: AggregatedError[],
+  activeSortFilter: SortFilters,
+  setActiveErrorSortFilter: (filter: SortFilters) => void,
   handleErrorSelect: (errorKey: string) => void
 }
 
-type Props = ConnectedProps & DispatchProps & DefaultsProps & RouteComponentProps<{service: string}>
+type Props = ConnectedProps & DispatchProps & DefaultsProps & RouteComponentProps<{ service: string }>
+
+export const SORT_FILTERS = {
+  "latest_occurrence": "Latest Occurence",
+  "event_count": "Event Count",
+}
 
 const sidebarItemClass = (error: AggregatedError): string => {
   if (error.severity === "info") {
@@ -38,19 +46,50 @@ const SideBar = (props: Props) => {
   const renderNavItems = () => {
     if (props.errors.length === 0) {
       return <div>no errors returned from api</div>
-    } else {
-      return props.errors.map((error, index) => {
-        return (
-          <ListGroup.Item action className={"sidebar-item" + " " + sidebarItemClass(error)} onClick={_ => props.handleErrorSelect(error.aggregation_key)} active={ props.activeError === undefined ? false : error.aggregation_key === props.activeError.aggregation_key } key={index}>
-            {error.aggregation_key} <Badge variant="secondary" className="float-right">{error.total_count}</Badge>
-          </ListGroup.Item>
-        )
-      })
     }
+
+    return props.errors.map((error, index) => {
+      return (
+        <ListGroup.Item
+          action className={"sidebar-item" + " " + sidebarItemClass(error)}
+          onClick={_ => props.handleErrorSelect(error.aggregation_key)}
+          active={props.activeError === undefined ? false : error.aggregation_key === props.activeError.aggregation_key}
+          key={index}
+        >
+          {error.aggregation_key} <Badge variant="secondary" className="float-right">{error.total_count}</Badge>
+        </ListGroup.Item>
+      )
+    })
+  }
+
+  const renderActions = () =>  {
+    return (
+      <div className='grid-component-actions'>
+        <DropdownButton
+          id="sort-btn"
+          as={ButtonGroup}
+          variant="secondary"
+          title={`Sort by: ${SORT_FILTERS[props.activeSortFilter]}`}
+          size="sm"
+        >
+          {Object.keys(SORT_FILTERS).map((filter: SortFilters) => (
+            <Dropdown.Item
+              key={`${filter}-sortFilter`}
+              active={filter === props.activeSortFilter}
+              onClick={_ => props.setActiveErrorSortFilter(filter)}
+            >
+              {SORT_FILTERS[filter]}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+      </div>
+      )
   }
 
   return (
     <div className="grid-component">
+      {renderActions()}
+
       <ListGroup>
         {renderNavItems()}
       </ListGroup>
@@ -59,12 +98,13 @@ const SideBar = (props: Props) => {
 }
 
 const matchDispatchToProps = (dispatch: Dispatch<AnyAction>): DispatchProps => {
-  return bindActionCreators({ setActiveError }, dispatch)
+  return bindActionCreators({ setActiveError, setActiveErrorSortFilter }, dispatch)
 }
 
 const mapStateToProps = (state: StoreState) => {
   return {
     activeError: state.errorsReducer.activeError,
+    activeSortFilter: state.errorsReducer.activeSortFilter,
   }
 }
 
