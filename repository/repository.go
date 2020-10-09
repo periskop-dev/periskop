626e2d1f4065c9a99d4a0b0368d1a496a8769801
Marc Tudurí <marc.tuduri@soundcloud.com>
2020-10-09 16:15:22 +0200

    Merge exceptions instead of swapping (#145)
    Co-authored-by: Ilya Mozerov <ilyailiilia@gmail.com>

--- repository/repository.go
+++ repository/repository.go
@@ -41,17 +41,22 @@ type ErrorsRepository interface {
 	GetServices() []string
 	GetErrors(serviceName string, numberOfErrors int) ([]ErrorAggregate, error)
 	ResolveError(serviceName string, key string) error
+	SearchResolved(serviceName string, key string) bool
+	RemoveResolved(serviceName string, key string)
 }
 
 func NewInMemory() ErrorsRepository {
 	return &inMemoryRepository{
 		AggregatedError: sync.Map{},
+		ResolvedErrors:  sync.Map{},
 	}
 }
 
 type inMemoryRepository struct {
-	// map service name -> errors
+	// map service name -> list of errors
 	AggregatedError sync.Map
+	// map service name -> set of resolved errors
+	ResolvedErrors sync.Map
 }
 
 func (r *inMemoryRepository) StoreErrors(serviceName string, errors []ErrorAggregate) {
@@ -87,6 +92,36 @@ func (r *inMemoryRepository) GetErrors(serviceName string, numberOfErrors int) (
 	return nil, fmt.Errorf("service %s not found", serviceName)
 }
 
+// addToResolved saves the error to resolved error set
+func (r *inMemoryRepository) addToResolved(serviceName string, key string) {
+	if resolvedSet, ok := r.ResolvedErrors.Load(serviceName); ok {
+		resolvedSet := resolvedSet.(map[string]bool)
+		resolvedSet[key] = true
+		r.ResolvedErrors.Store(serviceName, resolvedSet)
+	} else {
+		r.ResolvedErrors.Store(serviceName, map[string]bool{key: true})
+	}
+}
+
+// RemoveResolved removes a resolved error from resolved error set
+func (r *inMemoryRepository) RemoveResolved(serviceName string, key string) {
+	if resolvedSet, ok := r.ResolvedErrors.Load(serviceName); ok {
+		resolvedSet := resolvedSet.(map[string]bool)
+		delete(resolvedSet, key)
+		r.ResolvedErrors.Store(serviceName, resolvedSet)
+	}
+}
+
+// SearchResolved searches if an error is inside the set of resolved errors
+func (r *inMemoryRepository) SearchResolved(serviceName string, key string) bool {
+	if resolvedSet, ok := r.ResolvedErrors.Load(serviceName); ok {
+		resolvedSet := resolvedSet.(map[string]bool)
+		return resolvedSet[key]
+	}
+	return false
+}
+
+// ResolveError removes the error from list of errors and adds to the set of resolved errors
 func (r *inMemoryRepository) ResolveError(serviceName string, key string) error {
 	if value, ok := r.AggregatedError.Load(serviceName); ok {
 		value, _ := value.([]ErrorAggregate)
@@ -97,6 +132,7 @@ func (r *inMemoryRepository) ResolveError(serviceName string, key string) error 
 			}
 		}
 		r.StoreErrors(serviceName, newValues)
+		r.addToResolved(serviceName, key)
 		return nil
 	}
 	return fmt.Errorf("service %s not found", serviceName)
--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -6,20 +6,20 @@ import (
 	"testing"
 )
 
-func TestCombineErrorsSortsByTimestamp(t *testing.T) {
+func TestCombineLastErrorsSortsByTimestamp(t *testing.T) {
 	firstContent, _ := ioutil.ReadFile("sample-response1.json")
 	secondContent, _ := ioutil.ReadFile("sample-response2.json")
 
 	var firstResponsePayload responsePayload
 	var secondResponsePayload responsePayload
 
-	json.Unmarshal(firstContent, &firstResponsePayload)  // nolint[errcheck]
+	json.Unmarshal(firstContent, &firstResponsePayload)   // nolint[errcheck]
 	json.Unmarshal(secondContent, &secondResponsePayload) // nolint[errcheck]
 
 	firstOccurrences := firstResponsePayload.ErrorAggregate[0].LatestErrors
 	secondOccurrences := secondResponsePayload.ErrorAggregate[0].LatestErrors
 
-	result := combine(firstOccurrences, secondOccurrences)
+	result := combineLastErrors(firstOccurrences, secondOccurrences)
 
 	expectedUUIDs := []string{"uuid4", "uuid2", "uuid3", "uuid1"}
 
